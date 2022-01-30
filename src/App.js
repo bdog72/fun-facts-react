957eb86cead2314fce90dbbf96ffc843bd117c69
Brian Bycynski <high5bri@hotmail.com>
2022-01-30 03:28:23 -0500

    New Project

--- src/App.js
+++ src/App.js
@@ -1,30 +1,12 @@
 //
 //
 
-import './App.css';
+import React from 'react';
 
-// import List from './List';
-
-import imageReact from './react-logo.png';
-
-function App() {
+export default function App() {
   return (
-    <div className='app-container'>
-      <div className='app-container1'>
-        <img src={imageReact} alt='' />
-        <h1>Fun facts about React</h1>
-        {/* <List /> */}
-        <ul className='app-list'>
-          <li>Was first released in 2013</li>
-          <li>Was originally created by Jordan Walke</li>
-          <li>Has well over 100K stars on GitHub</li>
-          <li>Is maintained by Facebook</li>
-          <li>Powers thousands of enterprise apps</li>
-          <li>Including mobile apps</li>
-        </ul>
-      </div>
+    <div>
+      <h1>Bozo</h1>
     </div>
   );
 }
-
-export default App;
