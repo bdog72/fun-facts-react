f507c8690f064ab3e3307c6b862054418343c999
Brian Bycynski <high5bri@hotmail.com>
2022-01-31 02:45:26 -0500

    New Project

--- src/App.js
+++ src/App.js
@@ -3,10 +3,14 @@
 
 import React from 'react';
 
+import Main from './components/Main';
+import Navbar from './components/Navbar';
+
 export default function App() {
   return (
-    <div>
-      <h1>Bozo</h1>
+    <div className='container'>
+      <Navbar />
+      <Main />
     </div>
   );
 }
