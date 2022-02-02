637468d0834212e7a3acfda967dca52e53cfca9e
Brian Bycynski <high5bri@hotmail.com>
2022-02-02 04:27:59 -0500

    React

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,10 +1,21 @@
 //
 //
 
+import '../css/Main.css';
+
 export default function Main() {
   return (
-    <>
-      <h1>Main Bozo Boy 123</h1>
-    </>
+    <main className='main-container'>
+      <h1>Fun facts about React</h1>
+      <ul className='main-list'>
+        <li className=''>Was first released in 2013</li>
+        <li className=''>Was originally created by Jordan Walke</li>
+        <li className=''>Has well over 100k stars on Github</li>
+        <li className=''>Is maintained by Facebook</li>
+        <li className=''>
+          Powers thousands of enterprise apps, plus mobile apps
+        </li>
+      </ul>
+    </main>
   );
 }
